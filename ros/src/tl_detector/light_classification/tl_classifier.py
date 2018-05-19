240f4237ac3cd2d8cb7280376ad51f1ee42ea3a9
yingweiy@gmail.com <cdwy300l>
2018-05-19 11:29:29 -0700

    Bugfixing... Refactor the traffic light classifier code, and fine tuned the color range parameters. Improved the overall accuracy of yellow/green light classification.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -37,19 +37,19 @@ class TLClassifier(object):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         #  HUE Table
         #   Red:     0-10, 160-180
-        #   Yellow:  32-47    i.e., (45-67)/360*255
+        #   Yellow:  28-47    i.e., (40-67)/360*255
         #   Green:   63-99    i.e., (90-130)/360*255
         red_th1 = cv2.inRange(hsv, (0, 120, 120), (10, 255, 255))
         red_th2 = cv2.inRange(hsv, (160, 120, 120), (179, 255, 255))
         red_cnt = cv2.countNonZero(red_th1) + cv2.countNonZero(red_th2)
 
-        yellow_th = cv2.inRange(hsv, (32, 150, 150), (47, 255, 255))
+        yellow_th = cv2.inRange(hsv, (28, 120, 120), (47, 255, 255))
         yellow_cnt = cv2.countNonZero(yellow_th)
 
         green_th = cv2.inRange(hsv, (63, 100, 100), (92, 255, 255))
         green_cnt = cv2.countNonZero(green_th)
 
-        idx = np.argmax(np.array([red_cnt, yellow_cnt, green_cnt, 50]))
+        idx = np.argmax(np.array([red_cnt, green_cnt, yellow_cnt, 50]))
 
         if self.training_mode:
             self.saveimage(image, idx)
